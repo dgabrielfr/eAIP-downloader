62b0130b6c4f49df4d26ba56c0568d2c64edfc62
Damien GABRIEL <damien.gabriel.fr@gmail.com>
2020-07-29 19:25:49 +0200

    Added note to a possible bug

--- French_eAIP.py
+++ French_eAIP.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 # Python (3) script from maintening French eAIP information up-to date
-# Version 0.2a
+# Version 0.3
 # CHANGES
 # Read data from a CSV file
 # Should now return the current valid AIRAC instead of the nearest one in time (with may not have been released yet)
 # Save the previous AIRAC to a zip file
+# TODO: fix bug :
+#   when the airport.txt exists -> does not download the PDF files if missing
+#   So add a condition before writing the airport.txt file?
+#   The bug will not happens in normal use :)
 
 import datetime, urllib.request, urllib.error, os.path, bz2, shutil
 import pandas as pd
