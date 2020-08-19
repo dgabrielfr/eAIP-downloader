59f197ff789a745eaa55194356912cddd7ed8046
Damien GABRIEL <damien.gabriel.fr@gmail.com>
2020-08-19 08:01:07 +0200

    GUI not working! TODO: fix!

--- eAIP_downloader.py
+++ eAIP_downloader.py
@@ -7,34 +7,54 @@ import os
 from tkinter import *
 from tkinter.filedialog import *
 
-# Test
-def open_file_browser(filename):
-    filename = askopenfilename(title="Open folder", filetypes=[('text file', '.txt'), ('all files', '.*')])
-    print(filename)
-    return(filename)
+def browse_file(filename):
+    filename = askopenfilename(title="Open airport.txt file", filetypes=[('text file', '.txt'), ('all files', '.*')])
+    inp_browser.insert(0, filename)
 
-# GUI
-Window = Tk()
-folder_label = Label(Window, text="Choose folder path: ")
+def browse_folder(folder):
+    folder = askdirectory(title="Open folder")
+    inp_browser_folder.insert(0, folder)
+
 filename = ""
-label_browse = Label(Window, text="Press the button to browse")
-label_browse.pack()
-btn_browse = Button(Window, text="Browse", command=open_file_browser(filename))
-btn_browse.pack()
-value = StringVar() 
-value.set("texte par défaut")
-# entree = Entry(Window, textvariable=string, width=30)
-# entree.pack()
+folder = ""
 
-scriptpath = os.path.dirname(__file__)
-filename = os.path.join(scriptpath, "airac_date.txt")
+window = Tk()
+window.title = "GUI prototype"
+
+file_path = StringVar()
+folder_path = StringVar()
+
+# File Browser
+label_browser = Label(window, text="Press the button to browse to airport file:")
+label_browser.grid(row = 1, column = 1, padx=5, pady=5)
+# Need to use a lambda so as not to launch the function
+btn_browser = Button(window, text="Browse to file:", command=lambda: browse_file(filename))
+btn_browser.grid(row = 1, column=2, padx=5, pady=5)
+inp_browser = Entry(window)
+inp_browser.grid(row = 1, column = 3, padx=5, pady=5)
+# print(filename) # ""
 
 # Get AIRAC date and name as string
 eAIP_name_string = utm.latest_valid_AIRAC_name(filename)
 eAIP_date_string = utm.latest_valid_AIRAC_date(filename)
 
+# Folder browser
+label_folder_browser = Label(window, text="Press the button to browse to the download folder")
+label_folder_browser.grid(row = 2, column = 1, padx=5, pady=5)
+# Need to use a lambda so as not to launch the function
+btn_folder_browser = Button(window, text="Browse to folder:", command=lambda: browse_folder(folder))
+btn_folder_browser.grid(row = 2, column = 2, padx=5, pady=5)
+inp_browser_folder = Entry(window)
+inp_browser_folder.grid(row = 2, column = 3, padx=5, pady=5)
+inp_browser_folder.setvar(filename)
+
+# Download button
+btn_download = Button(window, text="Download", command=lambda: utm.download_french_metro_charts(fixed_path, folder))
+btn_download.grid(row = 3, column = 2, padx=5, pady=5)
+
 # Download, create and compress folder
-folder = os.path.join(scriptpath, "AIRAC " + eAIP_name_string)
+folder = inp_browser_folder.get()
+print(folder)
 airport_in = utm.read_airport_file(folder)
 if airport_in != -1:
     utm.download_airport_in_file(folder, airport_in, filename)
@@ -46,4 +66,4 @@ fixed_url_path = utm.fixed_french_reunion_download_url(filename)
 utm.download_french_reunion_charts(fixed_url_path, "AIRAC " + eAIP_name_string)
 utm.write_airport_file(folder)
 
-Window.mainloop()
+window.mainloop()
